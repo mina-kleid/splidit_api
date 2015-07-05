f2b4241f50f876584ed2b720e6f999575d64893c
Mina Kleid <minazakiz@gmail.com>
2015-07-05 15:32:21 +0200

    fixing migrations

--- db/migrate/20150704210356_add_is_admin_to_users_groups.rb
+++ db/migrate/20150704210356_add_is_admin_to_users_groups.rb
@@ -1,5 +1,5 @@
 class AddIsAdminToUsersGroups < ActiveRecord::Migration
   def change
-    add_column :user_groups, :is_admin, :boolean
+    add_column :users_groups, :is_admin, :boolean
   end
 end
