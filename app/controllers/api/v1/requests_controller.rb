d9d5b973582654c205fee3aa56b649f71a099975
Mina Kleid <minazakiz@gmail.com>
2016-01-16 16:00:15 +0200

    fixing request error

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::RequestsController < ApplicationController
     amount = permitted_params[:amount].to_d.abs
     post,request = RequestServiceObject.create(current_user,other_user,amount,conversation)
     APNS.send_notification(other_user.device_token, :alert => 'You have received a new post', :badge => 1, :sound => 'default',
-                           :other => {:conversation_id => @conversation.id}) unless other_user.device_token.nil?
+                           :other => {:conversation_id => conversation.id}) unless other_user.device_token.nil?
     render :json => {:post =>PostSerializer.new(post),:request => RequestSerializer.new(request)},:root => false and return
     #TODO show errors from request
   end
@@ -25,7 +25,7 @@ class Api::V1::RequestsController < ApplicationController
     if success
       other_user = conversation.other_user(current_user)
       APNS.send_notification(other_user.device_token, :alert => 'You have received a new post', :badge => 1, :sound => 'default',
-                             :other => {:conversation_id => @conversation.id}) unless other_user.device_token.nil?
+                             :other => {:conversation_id => conversation.id}) unless other_user.device_token.nil?
       render :json => {:post => PostSerializer.new(result),:user => {:balance => current_user.balance}},:root => false and return
     end
     return api_error(result)
@@ -38,7 +38,7 @@ class Api::V1::RequestsController < ApplicationController
     if success
       other_user = conversation.other_user(current_user)
       APNS.send_notification(other_user.device_token, :alert => 'You have received a new post', :badge => 1, :sound => 'default',
-                             :other => {:conversation_id => @conversation.id}) unless other_user.device_token.nil?
+                             :other => {:conversation_id => conversation.id}) unless other_user.device_token.nil?
       render :json => {:post => PostSerializer.new(result),:user => {:balance => current_user.balance}},:root => false and return
     end
     return api_error(result)
