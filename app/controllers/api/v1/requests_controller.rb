b36d312455c938d926887f8cfa6380856dc83e2a
Mina Kleid <minazakiz@gmail.com>
2016-01-16 15:18:00 +0200

    adding apn notifications

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -9,9 +9,11 @@ class Api::V1::RequestsController < ApplicationController
 
   def create
     conversation = current_user.conversations.find(params[:conversation_id])
-    other_user = conversation.users.where("id != ?",current_user.id).first
+    other_user = conversation.other_user(current_user)
     amount = permitted_params[:amount].to_d.abs
     post,request = RequestServiceObject.create(current_user,other_user,amount,conversation)
+    APNS.send_notification(other_user.device_token, :alert => 'You have received a new post', :badge => 1, :sound => 'default',
+                           :other => {:conversation_id => @conversation.id}) unless other_user.device_token.nil?
     render :json => {:post =>PostSerializer.new(post),:request => RequestSerializer.new(request)},:root => false and return
     #TODO show errors from request
   end
@@ -21,6 +23,9 @@ class Api::V1::RequestsController < ApplicationController
     conversation = current_user.conversations.where("user1_id = ? or user2_id = ?",@request.source_id,@request.source_id).first
     success, result = RequestServiceObject.accept(@request,conversation)
     if success
+      other_user = conversation.other_user(current_user)
+      APNS.send_notification(other_user.device_token, :alert => 'You have received a new post', :badge => 1, :sound => 'default',
+                             :other => {:conversation_id => @conversation.id}) unless other_user.device_token.nil?
       render :json => {:post => PostSerializer.new(result),:user => {:balance => current_user.balance}},:root => false and return
     end
     return api_error(result)
@@ -31,6 +36,9 @@ class Api::V1::RequestsController < ApplicationController
     conversation = current_user.conversations.where("user1_id = ? or user2_id = ?",@request.source_id,@request.source_id).first
     success, result = RequestServiceObject.reject(@request,conversation)
     if success
+      other_user = conversation.other_user(current_user)
+      APNS.send_notification(other_user.device_token, :alert => 'You have received a new post', :badge => 1, :sound => 'default',
+                             :other => {:conversation_id => @conversation.id}) unless other_user.device_token.nil?
       render :json => {:post => PostSerializer.new(result),:user => {:balance => current_user.balance}},:root => false and return
     end
     return api_error(result)
--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,10 +4,12 @@ class Api::V1::TransactionsController < ApplicationController
 
   def create
     conversation = current_user.conversations.find(params[:conversation_id])
-    other_user = conversation.users.where("id != ?",current_user.id).first
+    other_user = conversation.other_user(current_user)
     amount = permitted_params[:amount].to_d.abs
     success, result = TransactionServiceObject.create(current_user, other_user, amount, conversation)
     if success
+      APNS.send_notification(other_user(current_user).device_token, :alert => 'You have received money', :badge => 1, :sound => 'default',
+                             :other => {:conversation_id => conversation.id}) unless other_user.device_token.nil?
       render :json => {:post => PostSerializer.new(result), :user => {:balance => current_user.balance}},:root => false and return
     end
     return api_error(result)
