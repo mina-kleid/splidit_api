b8a9acc03df10dab0578d5ed1099d0d2cbd9926c
Mina Kleid <minazakiz@gmail.com>
2016-06-06 16:45:58 +0200

    more tests for posts and conversations

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :text, :post_type, :source_id, :amount
+  attributes :id, :text, :post_type, :source_id, :amount, :created_at
 
   def source_id
     object.user_id
--- spec/requests/conversations_controller_spec.rb
+++ spec/requests/conversations_controller_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 describe Api::V1::ConversationsController, type: :request do
 
 
-  let!(:user_1) {create(:user)}
-  let!(:user_2) {create(:user)}
+
 
   describe "Create new conversations" do
+
+    let!(:user_1) {create(:user)}
+    let!(:user_2) {create(:user)}
+
     it "should create a new conversation between two users" do
       post "/api/v1/conversations", {api_key:  api_key, conversation: {user_id: user_2.id}}, header_for_user(user_1)
       expect(response).to have_http_status(:success)
@@ -24,6 +27,8 @@ describe Api::V1::ConversationsController, type: :request do
 
   describe "fetches already existing conversation between users instead of creating a new one" do
 
+    let!(:user_1) {create(:user)}
+    let!(:user_2) {create(:user)}
     let!(:conversation) {create(:conversation, first_user: user_1, second_user: user_2)}
 
     it "should return the already existing conversation between users " do
@@ -42,9 +47,16 @@ describe Api::V1::ConversationsController, type: :request do
     end
   end
   describe "retrieves the conversation and its posts" do
-    let!(:conversation) {create(:conversation, :with_posts, first_user: user_1, second_user: user_2)}
-    it "should try" do
-      get "/api/v1/conversations/#{conversation.id}",{api_key:  api_key, conversation: {user_id: user_1.id, page: 1}}, header_for_user(user_1)
+
+    before(:all) do
+      @user_1 = create(:user)
+      @user_2 = create(:user)
+      @conversation = create(:conversation, :with_posts, first_user: @user_1, second_user: @user_2)
+      @page = 1
+      @first_post_id = @conversation.posts.first.id
+    end
+    it "should fetch the first page of conversations ordered chronologically" do
+      get "/api/v1/conversations/#{@conversation.id}",{api_key:  api_key, conversation: {user_id: @user_1.id, page: @page}}, header_for_user(@user_1)
       expect(response).to have_http_status(:success)
       parsed_body = JSON.parse(response.body)
       expect(parsed_body).not_to be_empty
@@ -52,6 +64,17 @@ describe Api::V1::ConversationsController, type: :request do
       expect(parsed_body["conversation"]).not_to be_empty
       expect(parsed_body["conversation"]["posts"]).not_to be_empty
       expect(parsed_body["conversation"]["posts"].count).to be(15)
+      posts = parsed_body["conversation"]["posts"]
+      expect(posts[0]["id"]).to eq(@first_post_id)
+      expect(posts[0]["created_at"]).to be > (posts[1]["created_at"])
+
+    end
+    it "should fetch the first page of conversations ordered chronologically" do
+      get "/api/v1/conversations/#{@conversation.id}",{api_key:  api_key, conversation: {user_id: @user_1.id, page: @page + 1}}, header_for_user(@user_1)
+      expect(response).to have_http_status(:success)
+      parsed_body = JSON.parse(response.body)
+      posts = parsed_body["conversation"]["posts"]
+      expect(posts[0]["id"]).to eq(@first_post_id - 15)
     end
   end
 end
