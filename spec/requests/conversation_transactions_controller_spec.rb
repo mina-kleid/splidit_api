af828789a2ee16850b9e7072425208c47783882d
Mina Kleid <minazakiz@gmail.com>
2016-06-22 15:50:48 +0200

    adding more tests for transactions

--- spec/requests/conversation_transactions_controller_spec.rb
+++ spec/requests/conversation_transactions_controller_spec.rb
@@ -20,7 +20,21 @@ describe Api::V1::ConversationRequestsController, type: :request do
       transaction = parsed_body["transaction"]
       post = parsed_body["post"]
       expect(transaction["amount"]).to eq(amount)
+      expect(post["amount"]).to eq(amount)
+      expect(transaction["transaction_type"]).to eq("debit")
+      expect(post["user_id"]).to eq(transaction["source"])
+      expect(post["post_type"]).to eq("transactions")
     end
 
+    it "should return an error when there is insufficient funds" do
+      amount = (user_1.account.balance + 0.01).to_s
+      post "/api/v1/conversations/#{conversation.id}/transactions", {api_key:  api_key, transaction: {amount: amount, text: "test"}}, header_for_user(user_1)
+      expect(response).to have_http_status(:bad_request)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).not_to be_empty
+      expect(parsed_body).to have_key("errors")
+      expect(parsed_body).not_to have_key("post")
+      expect(parsed_body).not_to have_key("transaction")
+    end
   end
 end
