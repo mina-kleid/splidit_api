8fbfbbf93ba9a6c1b2ae1da7f30c876e7e1594d2
Mina Kleid <minazakiz@gmail.com>
2016-06-29 12:00:16 +0200

    fixing syncing contacts with empty list

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,6 +4,9 @@ class Api::V1::ContactsController < ApplicationController
 
   def sync
     phone_numbers = permitted_params[:phone_numbers]
+    unless phone_numbers.present?
+      render :json => [], :each_serializer => ContactSerializer and return
+    end
     users = ContactsServiceObject.matched_users(phone_numbers).where.not(:phone => current_user.phone)
     render :json => users, :each_serializer => ContactSerializer and return
   end
--- spec/requests/contacts_controller_spec.rb
+++ spec/requests/contacts_controller_spec.rb
@@ -18,6 +18,16 @@ describe Api::V1::ContactsController, type: :request do
       expect(contacts.count).to eq(phone_numbers.count)
     end
 
+    it "should return an empty list if no contacts was given" do
+      post "/api/v1/contacts/sync",{api_key:  api_key, contacts: {phone_numbers: [nil]}}, header_for_user(user)
+      expect(response).to have_http_status(:success)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body).not_to be_empty
+      expect(parsed_body).not_to have_key("errors")
+      contacts = parsed_body["contacts"]
+      expect(contacts).to be_empty
+    end
+
   end
 
 end
