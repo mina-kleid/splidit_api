11f61a657db1c3f8910e3a9ddf238f8b32a6896f
Mina Kleid <minazakiz@gmail.com>
2016-07-06 15:00:06 +0200

    fixing the tests and making the ordering for the transactions

--- spec/requests/trasnactions_controller_spec.rb
+++ spec/requests/trasnactions_controller_spec.rb
@@ -6,8 +6,7 @@ describe Api::V1::TransactionsController, type: :request do
 
     let!(:user_1) {create(:user)}
     let!(:user_2) {create(:user)}
-    let!(:credit_transactions)  {create_list(:transaction, 5, :credit,target: user_1.account, source: user_2.account)}
-    let!(:debit_transactions)   {create_list(:transaction, 5, :debit, target: user_2.account, source: user_1.account)}
+    let!(:transactions_list)  {create_list(:transaction, 5, :credit, source: user_1.account, target: user_2.account)}
 
     it "sould list all the transactions from the user" do
       get "/api/v1/transactions/",{api_key:  api_key}, header_for_user(user_1)
@@ -15,8 +14,8 @@ describe Api::V1::TransactionsController, type: :request do
       parsed_body = JSON.parse(response.body)
       expect(parsed_body).to have_key("transactions")
       transactions = parsed_body["transactions"]
-      expect(transactions.size).to eq(credit_transactions.size + debit_transactions.size)
-      expect(transactions[0]["created_at"]).to be <=(transactions[1]["created_at"])
+      expect(transactions.size).to eq(transactions_list.count)
+      expect(transactions[0]["date"]).to be >=(transactions[1]["date"])
     end
 
   end
