c04f93144a66c614672855d6737cd5124098beaa
Mina Kleid <minazakiz@gmail.com>
2016-07-27 13:29:18 +0200

    new error handling and showing error messages

--- app/controllers/beta_users_controller.rb
+++ app/controllers/beta_users_controller.rb
@@ -12,10 +12,24 @@ class BetaUsersController < ActionController::Base
 
   def create
     @beta_user = BetaUser.new(permitted_params)
-    if @beta_user.save
+    if @beta_user.valid?
       mailchimp = Mailchimp::API.new(MAILCHIMP_API_KEY)
-      mailchimp.lists.subscribe(MAILCHIMP_LIST_ID, {email: @beta_user.email}, {FNAME: @beta_user.first_name, LNAME: @beta_user.last_name}, double_optin = false, send_welcome = false, update_existing = true)
-      redirect_to success_beta_users_path
+      begin
+        raise ""
+        mailchimp.lists.subscribe(MAILCHIMP_LIST_ID, {email: @beta_user.email}, {FNAME: @beta_user.first_name, LNAME: @beta_user.last_name}, double_optin = false, send_welcome = false, update_existing = true)
+        @beta_user.save
+        redirect_to success_beta_users_path
+      rescue Mailchimp::Error => e
+        if e.message.include?("is an invalid email address")
+          @beta_user.errors.add(:email, "Invalid email address, please enter a valid email")
+        else
+          @beta_user.errors.add(:base, e.message)
+        end
+      rescue StandardError => e
+        @beta_user.errors.add(:base, e.message)
+      ensure
+        render :new
+      end
     else
       render :new
     end
